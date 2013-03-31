7b9a20514db10bb0b4e2e20ec6493d5a7b9d22a3
Nick Monk <nick@monksoftware.co.uk>
2013-03-31 22:05:38 +0100

    Integration test to see if index page includes a link to registration

--- src/test/scala/com/plaintext/adapters/IndexPlanSpec.scala
+++ src/test/scala/com/plaintext/adapters/IndexPlanSpec.scala
@@ -1,48 +1,30 @@
 package com.plaintext.adapters
 
 import org.scalatest._
-
-trait RunningServer extends SequentialNestedSuiteExecution { this: Suite => 
-	import unfiltered.jetty._
-	
-	val host = Http.local(8080)
-	
-	def setup: (Server => Server) 
-    
-	lazy val server = setup(host)
-	
-	abstract override def withFixture(test: NoArgTest) {
-		server.start()
-		try {
-			super.withFixture(test)
-			
-		}
-		finally {
-			println("about to stop server")
-			Thread.sleep(200)
-			server.stop()
-			server.destroy()
-		}	
-	}
-	
-}
+import org.scalatest.matchers.ShouldMatchers._
     
 class IndexPlanSpec extends FlatSpec with RunningServer {
 	import dispatch._
  
  	def setup = {
-		println("in setup")
 		_.filter(new App)
 	}
     
 	"A request to index.html" should "return a 200 response" in {
 		val indexReq = url("http://localhost:8080") / "index.html"
+		val response = Http(indexReq)()
+		assert(response.getStatusCode() === 200)
+	}
 	
-		println("about to request")
-		for (str <- Http(indexReq OK as.String)) {
-			println(str)			
-		}		
-		val status = 404
-		assert(status === 200)
+	"A request to an unknown page" should "return a 404 response" in {
+		val unknownReq = url("http://localhost:8080") / "unknown.html"
+		val response = Http(unknownReq)()
+		assert(response.getStatusCode() === 404)
+	}
+	
+	"A request to index.html" should "return body content containing a link to the registration page" in {
+		val indexReq = url("http://localhost:8080") / "index.html"
+		val content = Http(indexReq OK as.String)()
+		content should include ("<a href=\"./register.html\">")		
 	}
 }
---
+++ src/test/scala/com/plaintext/adapters/RunningServer.scala
@@ -0,0 +1,23 @@
+package com.plaintext.adapters
+
+import unfiltered.jetty._
+import org.scalatest._
+
+trait RunningServer extends BeforeAndAfterAll { this: Suite => 
+	
+	val host = Http.local(8080)
+	
+	def setup: (Server => Server) 
+    
+	lazy val server = setup(host)
+	
+	override def beforeAll() {
+		server.start()
+	}
+	
+	override def afterAll() {
+		Thread.sleep(100)
+		server.stop()
+		server.destroy()
+	}
+}
